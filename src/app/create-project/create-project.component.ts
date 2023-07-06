713d7fdd76dd93bed42ec217bcddc5fa90bff484
Maxime <mbourgeois83120@gmail.com>
2023-07-06 15:36:24 +0200

    List of projects added

--- src/app/create-project/create-project.component.ts
+++ src/app/create-project/create-project.component.ts
@@ -4,6 +4,8 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/database';
 import { environment } from '../../environments/environment';
+import { Project } from './project.model';
+
 
 @Component({
   selector: 'app-create-project',
@@ -13,6 +15,7 @@ import { environment } from '../../environments/environment';
 export class CreateProjectComponent implements OnInit {
   userId: string = '';
   projectCreated: boolean = false; // Variable pour suivre l'état de création du projet
+  message: string = '';
 
   constructor(private route: ActivatedRoute, private router: Router) {}
 
@@ -23,10 +26,28 @@ export class CreateProjectComponent implements OnInit {
     });
   }
 
-  createProject(projectName: string) {
+  createProject(projectName: string, projectDescription: string, projectDate: string) {
     // Vérifiez si l'ID de l'utilisateur est disponible
     if (!this.userId) {
       console.error("ID de l'utilisateur non disponible.");
+      return;
+    }
+
+    // Vérifiez si tous les champs sont remplis
+    if (!projectName || !projectDescription || !projectDate) {
+      console.error("Veuillez remplir tous les champs.");
+      this.projectCreated = false; // Définir la variable projectCreated à false en cas d'erreur
+      this.message = "Veuillez remplir tous les champs.";
+      this.hideMessageAfterDelay(3000); // Masquer le message après 3 secondes
+      return;
+    }
+
+    // Vérifiez la longueur du nom du projet
+    if (projectName.length > 16) {
+      console.error("Le nom du projet ne peut pas dépasser 16 caractères.");
+      this.projectCreated = false; // Définir la variable projectCreated à false en cas d'erreur
+      this.message = "Le nom du projet ne peut pas dépasser 16 caractères.";
+      this.hideMessageAfterDelay(3000); // Masquer le message après 3 secondes
       return;
     }
 
@@ -46,19 +67,78 @@ export class CreateProjectComponent implements OnInit {
     const projectData = {
       id: projectId,
       name: projectName,
+      description: projectDescription,
+      date: projectDate,
+      groups: [] // Initialisez le champ "groups" à un tableau vide
       // Ajoutez d'autres propriétés du projet ici si nécessaire
     };
 
-    // Enregistrez le projet dans la base de données
-    newProjectRef.set(projectData)
+    newProjectRef
+      .set(projectData)
       .then(() => {
         console.log("Projet enregistré avec succès !");
-        this.projectCreated = true; // Met à jour l'état de création du projet
-        // Effectuez d'autres actions ou affichez un message de confirmation
+        this.projectCreated = true; // Définir la variable projectCreated à true
+        this.message = "Le projet a été créé avec succès !";
+        this.hideMessageAfterDelay(3000); // Masquer le message après 3 secondes
+
+        // Rediriger vers la page "list-of-projects" avec les paramètres de requête
+        this.router.navigate(['/list-of-projects'], {
+          queryParams: {
+            id: projectId,
+            name: projectName,
+            description: projectDescription,
+            date: projectDate
+          }
+        });
       })
       .catch((error) => {
         console.error("Erreur lors de l'enregistrement du projet :", error);
+        this.projectCreated = false; // Définir la variable projectCreated à false en cas d'erreur
         // Gérez l'erreur d'enregistrement du projet
       });
   }
+
+  hideMessageAfterDelay(delay: number) {
+    setTimeout(() => {
+      this.message = '';
+    }, delay);
+  }
+
+
+  getProjectsForUser(): Promise<Project[]> {
+    // Vérifiez si l'ID de l'utilisateur est disponible
+    if (!this.userId) {
+      console.error("ID de l'utilisateur non disponible.");
+      return Promise.reject("ID de l'utilisateur non disponible.");
+    }
+
+    // Créez une référence à la base de données Firebase
+    const db = firebase.database();
+
+    // Créez une référence à la collection de projets de l'utilisateur
+    const projectsRef = db.ref(`users/${this.userId}/projects`);
+
+    // Récupérez les projets de l'utilisateur en écoutant les modifications
+    return new Promise<Project[]>((resolve, reject) => {
+      projectsRef.on(
+        'value',
+        (snapshot) => {
+          const projects: Project[] = [];
+
+          snapshot.forEach((childSnapshot) => {
+            const project = childSnapshot.val() as Project;
+            projects.push(project);
+          });
+
+          resolve(projects);
+        },
+        (error) => {
+          console.error("Erreur lors de la récupération des projets :", error);
+          reject("Erreur lors de la récupération des projets");
+        }
+      );
+    });
+  }
+
+
 }
--- src/app/list-of-projects/list-of-projects.component.ts
+++ src/app/list-of-projects/list-of-projects.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChildren, QueryList } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
+import { environment } from '../../environments/environment';
+import { Project } from '../create-project/project.model';
+
 
 @Component({
   selector: 'app-list-of-projects',
@@ -7,29 +14,79 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 })
 export class ListOfProjectsComponent implements OnInit {
   userId: string = '';
+  status: string = '';
   items: HTMLElement[] = [];
-  active: number = 3;
+  active: number = 0;
+  projects: Project[] = [];
 
-  constructor(private elementRef: ElementRef) {}
+
+  constructor(private elementRef: ElementRef, private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
-    setTimeout(() => {
-      this.items = Array.from(this.elementRef.nativeElement.querySelectorAll('.slider .item'));
-      this.loadShow();
-
-      const nextButton = this.elementRef.nativeElement.querySelector('#next');
-      const prevButton = this.elementRef.nativeElement.querySelector('#prev');
-
-      nextButton.addEventListener('click', this.nextClick.bind(this));
-      prevButton.addEventListener('click', this.prevClick.bind(this));
+    firebase.initializeApp(environment.firebaseConfig);
+    this.route.params.subscribe(params => {
+      this.userId = params['userId']; // Récupère l'ID de l'utilisateur à partir des paramètres de route
     });
+
+    this.getProjectsForUser()
+      .then(projects => {
+        this.projects = projects;
+        console.log('Projets:', this.projects);
+        this.active = 0; // Réinitialiser l'index actif à 0
+
+        // Afficher le projet sélectionné dans la console
+        console.log('Projet sélectionné:', this.projects[this.active]);
+
+        this.loadShow(); // Appeler loadShow() une fois les projets récupérés
+
+        // Appeler loadShow() une deuxième fois après un court délai pour afficher correctement les projets dès le chargement de la page
+        setTimeout(() => {
+          this.loadShow();
+        }, 100);
+      })
+      .catch(error => {
+        console.error('Erreur lors de la récupération des projets:', error);
+        // Gérer l'erreur de récupération des projets
+      });
   }
 
+
+
+
+
   loadShow() {
+    this.items = Array.from(this.elementRef.nativeElement.querySelectorAll('.slider .item'));
+
     for (let i = 0; i < this.items.length; i++) {
       const item = this.items[i];
       const isActive = i === this.active;
 
+      const titleElement = item.querySelector('h1');
+      const descriptionElement = item.querySelector('p');
+
+      if (isActive && this.projects[this.active]) {
+        // Afficher le nom et la description du projet de l'élément actif s'ils sont définis
+        if (titleElement) {
+          titleElement.textContent = this.projects[this.active].name;
+        }
+        if (descriptionElement) {
+          descriptionElement.textContent = this.projects[this.active].description;
+        }
+        this.status = this.projects[this.active].status; // Mettez à jour la propriété status
+      } else {
+        // Afficher le nom et la description de chaque projet
+        if (titleElement) {
+          titleElement.textContent = this.projects[i].name;
+        }
+        if (descriptionElement) {
+          descriptionElement.textContent = this.projects[i].description;
+        }
+        this.status = ''; // Réinitialisez la propriété status
+      }
+
+      item.classList.toggle('active', isActive); // Ajouter ou supprimer la classe "active" en fonction de l'état du projet
+
+      // Appliquer les styles spécifiques pour les projets actifs ou inactifs
       item.style.transform = isActive ? 'none' : `translateX(${120 * (i - this.active)}px) scale(${1 - 0.2 * Math.abs(i - this.active)}) perspective(16px) rotateY(${isActive ? '0' : (i - this.active > 0 ? '-1' : '1')}deg)`;
       item.style.zIndex = isActive ? '1' : '0';
       item.style.opacity = isActive ? '1' : '0.6';
@@ -40,13 +97,61 @@ export class ListOfProjectsComponent implements OnInit {
 
 
 
+
+
+
+
+  getProjectsForUser(): Promise<Project[]> {
+    // Vérifiez si l'ID de l'utilisateur est disponible
+    if (!this.userId) {
+      console.error("ID de l'utilisateur non disponible.");
+      return Promise.reject("ID de l'utilisateur non disponible.");
+    }
+
+    // Créez une référence à la base de données Firebase
+    const db = firebase.database();
+
+    // Créez une référence à la collection de projets de l'utilisateur
+    const projectsRef = db.ref(`users/${this.userId}/projects`);
+
+    // Récupérez les projets de l'utilisateur en écoutant les modifications
+    return new Promise<Project[]>((resolve, reject) => {
+      projectsRef.on(
+        'value',
+        snapshot => {
+          const projects: Project[] = [];
+
+          snapshot.forEach(childSnapshot => {
+            const project = childSnapshot.val() as Project;
+            projects.push(project);
+          });
+
+          resolve(projects);
+        },
+        error => {
+          reject(error);
+        }
+      );
+    });
+  }
+
   nextClick() {
-    this.active = this.active + 1 < this.items.length ? this.active + 1 : this.active;
+    const nextIndex = (this.active + 1) % this.projects.length;
+    this.active = nextIndex;
     this.loadShow();
+    console.log('Projet sélectionné:', this.projects[this.active]);
   }
 
   prevClick() {
-    this.active = this.active - 1 >= 0 ? this.active - 1 : this.active;
+    const prevIndex = (this.active - 1 + this.projects.length) % this.projects.length;
+    this.active = prevIndex;
     this.loadShow();
+    console.log('Projet sélectionné:', this.projects[this.active]);
   }
+
+  navigateToProjectDetails(projectId: string) {
+    this.router.navigate(['/project', projectId]);
+  }
+
+
 }
